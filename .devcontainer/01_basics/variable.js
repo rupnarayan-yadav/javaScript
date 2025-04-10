2baaef359855d4f8e4bedae3c7895a6ccc8e6eab
Rup Narayan Yadav <rupnarayan.23ise@cambridge.edu.in>
2025-04-10 18:27:16 +0000

    variable

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 13453452
+let accountemail = "hehuweeh@gmail.com"
+var accountpassword = "2345663"
+accountCity = "banglore"
+
+// accountId = 2  not allowed  can't modify cont again
+
+accountemail = "wwdj@hc.com"
+accountpassword = "766375"
+accountCity = "janakpur"
+
+
+
+console.log(accountId);
+console.table([accountId, accountemail, accountpassword, accountCity])
+
+
+// prefare not to use var because of
+// issue in block scope and functional scope
