e0fadfd95983c96fc09b34aa487f57a57ae849fd
Rupnarayan-yadav <rupnarayan.23ise@cambridge.edu.in>
2025-04-12 00:44:01 +0530

    stack and heap

---
+++ 01_basics/StackAndHeap.js
@@ -0,0 +1,20 @@
+let myYoutubename = "rupeshkumar.com"
+
+let anothername = myYoutubename
+anothername = "shivamkumar"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email: "user@google.com",
+    upi: "user@bl"
+}
+
+let userTwo = userOne
+
+
+userTwo.email = "rupesh@goooglr.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
