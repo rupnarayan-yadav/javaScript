12fcfa0b4c2b13abbdd1c4c150dab454636f6a20
rupnarayan-yadav <rupnarayan.23ise@cambridge.edu.in>
2025-04-19 19:16:05 +0530

    for in loop

---
+++ control_flow/for_in.js
@@ -0,0 +1,17 @@
+const myobject={
+    js:`javascript`,
+    cpp:`c++`,
+    rb:`ruby`,
+    swift:`swift by apple`
+}
+for(const key in myobject){
+    //console.log(myobject[key])
+    //console.log(key)
+    //console.log(`${key} shortcut is for ${myobject[key]}`);
+}
+
+const programming = ["js", "rb", "pb", "java", "cpp"]
+for(const key in programming){
+    //console.log(key)  it 
+    //console.log(programming[key])
+}
